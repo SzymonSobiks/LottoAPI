77e5513c425c8767535cc2f3470d1a852b83e3e7
Szymon Sobik <szymon.sobik13@gmail.com>
2022-05-30 22:00:23 +0200

    Shared service setup

--- ui/LottoUI/src/app/shared.service.ts
+++ ui/LottoUI/src/app/shared.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
+  
+  readonly APIUrl = "http://localhost:5001/api";
 
-  constructor() { }
+
+  constructor(private http:HttpClient) { }
+
+getDrawList():Observable<any[]>{
+  return this.http.get<any>(this.APIUrl+'/draw');
+}
+
+addDraw(val:any){
+  return this.http.post(this.APIUrl+'/draw',val);
+}
+
 }
