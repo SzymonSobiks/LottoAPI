c21da0c97ddde3e95710147aaa13e92f723edcf5
Szymon Sobik <szymon.sobik13@gmail.com>
2022-05-30 20:54:32 +0200

    DrawController setup
    -install sqlclient package
    -add HttpGet method

---
+++ LottoAPI/Controllers/DrawController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace LottoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrawController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DrawController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                      select DrawId, DrawNumber, DrawDateTime from dbo.DrawHistory";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("LotteryAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+
+
+    }
+}
