9ff581cf15e2813ca4a96a9f1b72cc990305a588
Szymon Sobik <szymon.sobik13@gmail.com>
2022-05-30 20:42:21 +0200

    Adds class Draw

---
+++ LottoAPI/Models/Draw.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LottoAPI.Models
+{
+    public class Draw
+    {
+        public int Id { get; set; }
+        public int[] Numbers { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public string GetTimestampAsString() { return Timestamp.ToString(); }
+    }
+}
