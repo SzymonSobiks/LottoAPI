dad4ed99a17b7b15bd1db5feb6143cde884f0555
Szymon Sobik <szymon.sobik13@gmail.com>
2022-05-31 14:56:47 +0200

    Remove unnecessary showlast component

--- ui/LottoUI/src/app/app.module.ts
+++ ui/LottoUI/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewdrawComponent } from './newdraw/newdraw.component';
-import { ShowlastDrawComponent } from './newdraw/showlast-draw/showlast-draw.component';
 import { AddDrawComponent } from './newdraw/add-draw/add-draw.component';
 import { DrawhistoryComponent } from './drawhistory/drawhistory.component';
 import { SharedService } from './shared.service';
@@ -16,7 +15,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   declarations: [
     AppComponent,
     NewdrawComponent,
-    ShowlastDrawComponent,
     AddDrawComponent,
     DrawhistoryComponent
   ],
